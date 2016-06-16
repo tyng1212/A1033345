59e15cbb3b4374b3d4274788191da22c817cb070
tyng1212 <tyng1212@users.noreply.github.com>
2016-06-16 11:57:46 +0800

    Add files via upload

--- index1.php
+++ index1.php
@@ -1,20 +1,12 @@
 <?php
 
 class PostOffice{
-	/*function mailFiler(){
-		$fp=fopen("string.txt","r");
-		fscanf($fp, "%s %s", $n, $str);
-		fclose($fp);
-		echo "$n $str";             
-	}*/
-
 	function mailFiler(){
 		$filename="string.txt";
 		$fp=fopen($filename,"r");
 		$contents=fread($handle,filsize($filename));
 		fclose($handle);             
 	}
-
 
 	function mailRegex(){
 		$fp=fopen("string.txt","r");
@@ -27,24 +19,6 @@ class PostOffice{
 	function spiltroad(){
 
 	}
-/*
-	public function mailFiler(){
-		$filename = ‘string.txt’;
-		$fp = fopen($filename, ‘r’);
-		fread($fp, iconv("UTF-8", "BIG5", "測試文件"));
-		fclose($fp);                  
-	}
-
-	public function mailFiler(){
-		$filename = ‘string.txt’;
-		$fp = fopen($filename, ‘r’);
-		fread($fp, iconv("UTF-8", "BIG5", "測試文件"));
-		fclose($fp);                  
-	}	
-	*/
 }
-
-
-
 
 ?>
